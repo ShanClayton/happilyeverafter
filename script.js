45d8bc8949c9d4f354ed19ab04b1945afdbc7492
Shanaun <sclayton1183@gmail.com>
2019-05-17 00:41:41 -0700

    added new nav bar

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
 // NAV BAR
-$(document).ready(function(){
-    $(window).on('scroll',function(){
-        if($(window).scrollTop()){
-            $(".sub-header").addClass('active');
-        }else{
-            $(".sub-header").removeClass('active');
-        }
-    });
-});
+
+var isActive = false;
+
+$('.menu').on('click', function(){
+  if(isActive){
+    $(this).removeClass('active');
+    $('body').removeClass('menu-open');
+  }else{
+    $(this).addClass('active');
+    $('body').addClass('menu-open')
+  }
+  isActive = !isActive;
+})
+// $(document).ready(function(){
+//     $(window).on('scroll',function(){
+//         if($(window).scrollTop()){
+//             $(".sub-header").addClass('active');
+//         }else{
+//             $(".sub-header").removeClass('active');
+//         }
+//     });
+// });
 
 //MODAL
-const openBtn = document.getElementById('modalOpen');
-const closeBtn = document.getElementById('modalClose');
-
-const modal = document.getElementById('theModal');
-
-openBtn.addEventListener('click', () =>{
-  modal.classList.add('visible');
-});
-
-closeBtn.addEventListener('click', () => {
-  modal.classList.remove('visible');
-})
+// const openBtn = document.getElementById('modalOpen');
+// const closeBtn = document.getElementById('modalClose');
+//
+// const modal = document.getElementById('theModal');
+//
+// openBtn.addEventListener('click', () =>{
+//   modal.classList.add('visible');
+// });
+//
+// closeBtn.addEventListener('click', () => {
+//   modal.classList.remove('visible');
+// })
 
 // GOOGLE MAPS
